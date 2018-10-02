fd88b48423b2059118c4147e61b8af5c8addfc24
Gutierrez PS <gutierrez@live.jp>
2018-10-02 20:42:21 -0300

    Added LCD basics

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
  */
 
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+//              lcd(rs, en, d4, d5, d6, d7)
+LiquidCrystal g_lcd( 4,  5,  6,  7,  8,  9);
 
 void setup() {
-    // put your setup code here, to run once:
+    g_lcd.begin(16, 2);
+    g_lcd.clear();
+    g_lcd.print("  hello world!");
 }
 
 void loop() {
