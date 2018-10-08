183f3edcde2c5db3477774b759b448fb3fb7bfb5
Gutierrez PS <gutierrez@live.jp>
2018-10-08 18:48:19 -0300

    Add mode selection and current offset calibration for each mode

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,7 @@
 //              lcd(rs, en, d4, d5, d6, d7)
 LiquidCrystal g_lcd( 4,  5,  6,  7,  8,  9);
 
-const struct
-{
+const struct {
     char chargeVoltage;
     char dischargeVoltage;
     char floatVoltage;
@@ -24,21 +23,21 @@ const struct
     char modeButton;
     char chargeOrDischargeRelay;
     char connectOrFloatRelay;
-} k_pins = {A0, A1, A2, A3, 9, 10, 11};
+} k_pins = {A0, A1, A2, A3, 10, 11, 12};
 
 const char k_nAnalogSamples = 100;       // to take for each acquisition
+const float k_modeFactor[3] = { 3.04, 3.249, 65.57 };   // voltade divider factor
 
 
 /**
  * Global variables
  **/
 
-float g_currentSensorOffset = 2.5;      // voltage value for 0 Amperes
 unsigned long g_lcdTimer = 0;
+float g_currentSensorOffset[3] = {2.5, 2.5, 2.5};   // voltage values for 0 Amperes
 
-char g_currentMode = 1;         // 0: discharge, 1: float, 2: charging
+uint8_t g_currentMode = 1;         // 0: discharge, 1: float, 2: charging
 const char k_modeStrings[3][6] = {"DISCH", "FLOAT", "CHARG"};
-const float k_modeFactor[3] = { 3.04, 3.249, 65.57 };
 
 /**
  * Better precision analogRead()
@@ -48,7 +47,7 @@ const float k_modeFactor[3] = { 3.04, 3.249, 65.57 };
 float avgAnalog(char pin) {
     unsigned long sum = 0;
 
-    for (char i = 0; i < k_nAnalogSamples; ++i)
+    for (uint8_t i = 0; i < k_nAnalogSamples; ++i)
     {
         sum += analogRead(pin);
         delayMicroseconds(25);
@@ -57,6 +56,18 @@ float avgAnalog(char pin) {
     return (sum/(k_nAnalogSamples * 1023.0)) * 5.0;
 }
 
+void changeModeRelays() {
+    if (g_currentMode != 1) {
+        digitalWrite(k_pins.connectOrFloatRelay, HIGH);
+        if(g_currentMode == 0) {
+            digitalWrite(k_pins.chargeOrDischargeRelay, LOW);
+        } else {
+            digitalWrite(k_pins.chargeOrDischargeRelay, HIGH);
+        }
+    } else {
+        digitalWrite(k_pins.connectOrFloatRelay, LOW);
+    }
+}
 
 void setup() {
     pinMode(k_pins.modeButton, INPUT_PULLUP);
@@ -71,38 +82,56 @@ void setup() {
 
     
     // Current sensor offset calibration
+    delay(1000);
+    
+    // calibrate float offset
+    g_currentSensorOffset[1] = avgAnalog(k_pins.currentSensor);
+    g_lcd.print('.');
 
-    int count = 0;
-    unsigned long sum = 0;
-    unsigned long calibrationTimer = millis(), dotsTimer = millis();
+    // calibrate discharge offset
+    g_currentMode = 0;
+    changeModeRelays();
+    delay(1000);
+    g_currentSensorOffset[0] = avgAnalog(k_pins.currentSensor);
+    g_lcd.print('.');
 
-    while (millis() - calibrationTimer < 2000) {
-        sum += analogRead(k_pins.currentSensor);
-        count++;
-        delay(10);
-
-        if (millis() - dotsTimer > 400) {
-            g_lcd.print('.');
-            dotsTimer = millis();
-        }
-    }
-
-    g_currentSensorOffset = (sum/(count * 1023.0)) * 5.0;
+    // calibrate charge offset
+    g_currentMode = 2;
+    changeModeRelays();
+    delay(1000);
+    g_currentSensorOffset[2] = avgAnalog(k_pins.currentSensor);
+    g_lcd.print('.');
 
     g_lcd.clear();
 }
 
 void loop() {
+
+    // change operating mode when button is pressed
+    if (digitalRead(k_pins.modeButton) == LOW) {
+        delay(50);
+        while (digitalRead(k_pins.modeButton) == LOW) delay(1);
+        delay(50);
+
+        if (++g_currentMode == 3) {
+            g_currentMode = 0;
+        }
+
+        changeModeRelays();
+    }
     
     // update LCD display every 1 second
     if (millis() - g_lcdTimer < 1000) return;
 
     g_lcdTimer = millis();
 
-    char mode = g_currentMode;
+    uint8_t mode = g_currentMode;
 
     g_lcd.setCursor(0,0);
-    g_lcd.print(k_modeStrings[mode]);
+    g_lcd.print(k_modeStrings[mode]);       // 5 chars
+
+    g_lcd.setCursor(7, 0);
+    
 
 
     // print voltage
@@ -113,7 +142,8 @@ void loop() {
     if (mode == 1) readFrom = k_pins.floatVoltage;
     else if (mode == 2) readFrom = k_pins.chargeVoltage;
 
-    float voltage = avgAnalog(readFrom) * k_modeFactor[mode];
+    float analogValue = avgAnalog(readFrom);
+    float voltage = analogValue * k_modeFactor[mode];
     g_lcd.print(voltage, 2);
     g_lcd.print(" V    ");
 
@@ -123,7 +153,7 @@ void loop() {
     g_lcd.setCursor(10, 1);
 
     float current = avgAnalog(k_pins.currentSensor);
-    current = (current - g_currentSensorOffset) * 0.066;    // 2.5V offset, 66mV/A
+    current = (current - g_currentSensorOffset[mode]) / 0.185;    // 2.5V offset, 185mV/A
     if (current < 0) current *= -1.0;
 
     g_lcd.print(current);
